45ce053859ce1b939e860bf40fa6d20bbeedbed5
ACHAVA <ACHAVA-C@USTEXAUS612957.dot.state.tx.us>
2019-01-10 17:51:39 -0600

    Initial commit. Different console.logs to use for debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/interval';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('input') button: ElementRef;
+
+  ngOnInit(): void {
+
+    /** console statements */
+    // console.error(new Error('Whoops, something bad happened'));
+    // console.time('a');
+    // const name = 'Will Robinson';
+    // console.warn(`Danger ${name}! Danger!`);
+    // console.log('hello world');
+    // console.log('hello %d', 'world');
+    // console.assert(String(2) == String(2), 'Whoops %s work', 'didn\'t');
+    // let array = [3, 6];
+    // let elements = [0, 1, 2];
+    // array.reverse.apply(array);
+    // console.log(Math.max.apply(null, array))
+    // console.info(array)
+    // console.timeEnd('a')
+
+    /**
+     * Observables
+     */
+    // fromEvent
+    Observable
+      .fromEvent(this.button.nativeElement, 'click')
+      .subscribe(
+        (data) => { console.log('qwewq'); console.log(data); }
+    );
+
+    // interval
+
+  }
 }
